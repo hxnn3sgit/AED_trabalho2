11f984abb7557980a5e7c344fe78b6450de4faf6
hxnn3sgit <seihannes@gmx.de>
2025-01-02 19:11:33 +0000

    added eccentrity measures

--- Trabalho_2_Ficheiros_para_os_ESTUDANTES/GraphEccentricityMeasures.c
+++ Trabalho_2_Ficheiros_para_os_ESTUDANTES/GraphEccentricityMeasures.c
@@ -1,108 +1,128 @@
-//
 // Algoritmos e Estruturas de Dados --- 2024/2025
 //
 // Joaquim Madeira - Dec 2024
 //
 // GraphEccentricityMeasures
-//
-
-// Student Name :
-// Student Number :
-// Student Name :
-// Student Number :
-
-/*** COMPLETE THE GraphEccentricityMeasuresCompute FUNCTION ***/
-/*** COMPLETE THE GraphGetCentralVertices FUNCTION ***/
-/*** COMPLETE THE GraphEccentricityMeasuresPrint FUNCTION ***/
 
 #include "GraphEccentricityMeasures.h"
-
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "Graph.h"
 #include "GraphAllPairsShortestDistances.h"
 
 struct _GraphEccentricityMeasures {
-  unsigned int*
-      centralVertices;  // centralVertices[0] = number of central vertices
-                        // array size is (number of central vertices + 1)
-  int* eccentricity;    // the eccentricity value of each vertex
-  Graph* graph;         // the graph
-  int graphRadius;      // the graph radius
-  int graphDiameter;    // the graph diameter
+  unsigned int* centralVertices;  // centralVertices[0] = number of central vertices
+                                  // array size is (number of central vertices + 1)
+  int* eccentricity;              // the eccentricity value of each vertex
+  Graph* graph;                   // the graph
+  int graphRadius;                // the graph radius
+  int graphDiameter;              // the graph diameter
 };
 
-// Allocate memory
-// Compute the vertex eccentricity values
-// Compute graph radius and graph diameter
-// Compute the set of central vertices
 GraphEccentricityMeasures* GraphEccentricityMeasuresCompute(Graph* g) {
-  assert(g != NULL);
+    assert(g != NULL);
 
-  // COMPLETE THE CODE
-  // CREATE AUXILIARY (static) FUNCTIONS, IF USEFUL
-  // Graph radius --- the smallest vertex eccentricity value
-  // Graph diameter --- the largest vertex eccentricity value
-  // Do not forget that -1 represents an IDEFINITE value
+    unsigned int numVertices = GraphGetNumVertices(g);
+    GraphAllPairsShortestDistances* allPairs = GraphAllPairsShortestDistancesExecute(g);
 
-  // Computing the set of central vertices
-  // Allocate the central vertices array : number of central vertices + 1
-  // Fill in the central vertices array
+    GraphEccentricityMeasures* measures = malloc(sizeof(GraphEccentricityMeasures));
+    assert(measures != NULL);
 
-  return NULL;
+    measures->graph = g;
+    measures->eccentricity = malloc(sizeof(int) * numVertices);
+    assert(measures->eccentricity != NULL);
+
+    measures->graphRadius = INT_MAX;
+    measures->graphDiameter = 0;
+
+    // Compute eccentricities, radius, and diameter
+    for (unsigned int i = 0; i < numVertices; ++i) {
+        int maxDistance = 0;
+        for (unsigned int j = 0; j < numVertices; ++j) {
+            int distance = GraphGetDistanceVW(allPairs, i, j);
+            if (distance > maxDistance) {
+                maxDistance = distance;
+            }
+        }
+        measures->eccentricity[i] = maxDistance;
+        if (maxDistance < measures->graphRadius) {
+            measures->graphRadius = maxDistance;
+        }
+        if (maxDistance > measures->graphDiameter) {
+            measures->graphDiameter = maxDistance;
+        }
+    }
+
+    // Compute central vertices
+    unsigned int centralCount = 0;
+    for (unsigned int i = 0; i < numVertices; ++i) {
+        if (measures->eccentricity[i] == measures->graphRadius) {
+            ++centralCount;
+        }
+    }
+
+    measures->centralVertices = malloc(sizeof(unsigned int) * (centralCount + 1));
+    assert(measures->centralVertices != NULL);
+
+    measures->centralVertices[0] = centralCount;
+    unsigned int index = 1;
+    for (unsigned int i = 0; i < numVertices; ++i) {
+        if (measures->eccentricity[i] == measures->graphRadius) {
+            measures->centralVertices[index++] = i;
+        }
+    }
+
+    GraphAllPairsShortestDistancesDestroy(&allPairs);
+    return measures;
 }
 
 void GraphEccentricityMeasuresDestroy(GraphEccentricityMeasures** p) {
-  assert(*p != NULL);
+    assert(p != NULL && *p != NULL);
 
-  GraphEccentricityMeasures* aux = *p;
+    GraphEccentricityMeasures* measures = *p;
+    free(measures->eccentricity);
+    free(measures->centralVertices);
+    free(measures);
 
-  free(aux->centralVertices);
-  free(aux->eccentricity);
-
-  free(*p);
-  *p = NULL;
+    *p = NULL;
 }
 
-// Getting the computed measures
-
 int GraphGetRadius(const GraphEccentricityMeasures* p) {
-  assert(p != NULL);
-
-  return p->graphRadius;
+    assert(p != NULL);
+    return p->graphRadius;
 }
 
 int GraphGetDiameter(const GraphEccentricityMeasures* p) {
-  assert(p != NULL);
-
-  return p->graphDiameter;
+    assert(p != NULL);
+    return p->graphDiameter;
 }
 
-int GraphGetVertexEccentricity(const GraphEccentricityMeasures* p,
-                               unsigned int v) {
-  assert(p != NULL);
-  assert(v < GraphGetNumVertices(p->graph));
-  assert(p->eccentricity != NULL);
-
-  return p->eccentricity[v];
+int GraphGetVertexEccentricity(const GraphEccentricityMeasures* p, unsigned int v) {
+    assert(p != NULL);
+    assert(v < GraphGetNumVertices(p->graph));
+    return p->eccentricity[v];
 }
 
-// Getting a copy of the set of central vertices
-// centralVertices[0] = number of central vertices in the set
 unsigned int* GraphGetCentralVertices(const GraphEccentricityMeasures* p) {
-  assert(p != NULL);
-  assert(p->centralVertices != NULL);
-
-  // COMPLETE THE CODE
-
-  return NULL;
+    assert(p != NULL);
+    return p->centralVertices;
 }
 
-// Print the graph radius and diameter
-// Print the vertex eccentricity values
-// Print the set of central vertices
 void GraphEccentricityMeasuresPrint(const GraphEccentricityMeasures* p) {
-  // COMPLETE THE CODE
+    assert(p != NULL);
+
+    printf("Graph Radius: %d\n", p->graphRadius);
+    printf("Graph Diameter: %d\n", p->graphDiameter);
+
+    printf("Vertex Eccentricities:\n");
+    for (unsigned int i = 0; i < GraphGetNumVertices(p->graph); ++i) {
+        printf("Vertex %u: %d\n", i, p->eccentricity[i]);
+    }
+
+    printf("Central Vertices: ");
+    for (unsigned int i = 1; i <= p->centralVertices[0]; ++i) {
+        printf("%u ", p->centralVertices[i]);
+    }
+    printf("\n");
 }
--- Trabalho_2_Ficheiros_para_os_ESTUDANTES/TestBellmanFordAlg.c
+++ Trabalho_2_Ficheiros_para_os_ESTUDANTES/TestBellmanFordAlg.c
@@ -13,115 +13,125 @@
 #include "instrumentation.h"
 
 int main(void) {
-  // What kind of graph is dig01?
-  Graph* dig01 = GraphCreate(6, 1, 0);
-  GraphAddEdge(dig01, 1, 2);
-  GraphAddEdge(dig01, 1, 4);
-  GraphAddEdge(dig01, 3, 4);
-  printf("The graph:\n");
-  // Displaying in DOT format
-  GraphDisplayDOT(dig01);
-  printf("\n");
+    // What kind of graph is dig01?
+    Graph* dig01 = GraphCreate(6, 1, 0);
+    GraphAddEdge(dig01, 1, 2);
+    GraphAddEdge(dig01, 1, 4);
+    GraphAddEdge(dig01, 3, 4);
 
-  GraphCheckInvariants(dig01);
-
-  // Bellman-Ford Algorithm
-
-  // Consider each vertex as a start vertex
-  for (unsigned int i = 0; i < 6; i++) {
-    InitializeInstrumentation();
-    InstrCalibrate();
-    GraphBellmanFordAlg* BF_result = GraphBellmanFordAlgExecute(dig01, i);
-
-    printf("The shortest path tree rooted at %u\n", i);
-    GraphBellmanFordAlgDisplayDOT(BF_result);
+    printf("The graph:\n");
+    // Displaying in DOT format
+    GraphDisplayDOT(dig01);
     printf("\n");
 
-    // Get memory usage for BF_result
-    size_t numVertices = GraphGetNumVertices(dig01); // Number of vertices remains the same
-    size_t numEdges = 0;
+    GraphCheckInvariants(dig01);
 
-    for (unsigned int j = 0; j < numVertices; ++j) {
-      if (GraphBellmanFordAlgReached(BF_result, j) && GraphBellmanFordAlgGetPredecessor(BF_result, j) != -1) {
-        numEdges++;
-      }
+    // Bellman-Ford Algorithm
+    // Consider each vertex as a start vertex
+    for (unsigned int i = 0; i < 6; i++) {
+        InitializeInstrumentation();
+        InstrCalibrate();
+
+        double startTime = cpu_time();
+        GraphBellmanFordAlg* BF_result = GraphBellmanFordAlgExecute(dig01, i);
+        double endTime = cpu_time();
+
+        printf("The shortest path tree rooted at %u\n", i);
+        GraphBellmanFordAlgDisplayDOT(BF_result);
+        printf("\n");
+
+        // Get memory usage for BF_result
+        size_t numVertices = GraphGetNumVertices(dig01); // Number of vertices remains the same
+        size_t numEdges = 0;
+
+        for (unsigned int j = 0; j < numVertices; ++j) {
+            if (GraphBellmanFordAlgReached(BF_result, j) && GraphBellmanFordAlgGetPredecessor(BF_result, j) != -1) {
+                numEdges++;
+            }
+        }
+
+        size_t vertexSize = sizeof(unsigned int) + sizeof(int) * 2; // marked, distance, predecessor
+        size_t edgeSize = sizeof(int);
+        size_t memoryUsage = EstimateMemoryUsage(numVertices, numEdges, vertexSize, edgeSize);
+
+        printf("Execution Time: %.6f seconds\n", endTime - startTime);
+        PrintInstrumentationMetrics("BellmanFord Algorithm", memoryUsage);
+
+        InstrReset();
+        GraphBellmanFordAlgDestroy(&BF_result);
     }
 
-    size_t vertexSize = sizeof(unsigned int) + sizeof(int) * 2;
-    size_t edgeSize = sizeof(int);
-    size_t memoryUsage = EstimateMemoryUsage(numVertices, numEdges, vertexSize, edgeSize);
+    // What kind of graph is g01?
+    Graph* g01 = GraphCreate(6, 0, 0);
+    GraphAddEdge(g01, 0, 5);
+    GraphAddEdge(g01, 2, 4);
+    GraphAddEdge(g01, 2, 3);
+    GraphAddEdge(g01, 1, 2);
+    GraphAddEdge(g01, 0, 1);
+    GraphAddEdge(g01, 3, 4);
+    GraphAddEdge(g01, 3, 5);
+    GraphAddEdge(g01, 0, 2);
 
-    PrintInstrumentationMetrics("BellmanFord Algorithm", memoryUsage);
-    
+    printf("The graph:\n");
+    // Displaying in DOT format
+    GraphDisplayDOT(g01);
+    printf("\n");
+
+    GraphCheckInvariants(g01);
+
+    // Bellman-Ford Algorithm
+    InitializeInstrumentation();
+
+    // Consider each vertex as a start vertex
+    for (unsigned int i = 0; i < 6; i++) {
+        double startTime = cpu_time();
+        GraphBellmanFordAlg* BF_result = GraphBellmanFordAlgExecute(g01, i);
+        double endTime = cpu_time();
+
+        printf("The shortest path tree rooted at %u\n", i);
+        GraphBellmanFordAlgDisplayDOT(BF_result);
+        printf("\n");
+
+        printf("Execution Time: %.6f seconds\n", endTime - startTime);
+        GraphBellmanFordAlgDestroy(&BF_result);
+    }
+
+    InstrPrint();
+
+    // Reading a directed graph from file
+    FILE* file = fopen("DG_2.txt", "r");
+    Graph* dig03 = GraphFromFile(file);
+    fclose(file);
+
+    // Displaying in DOT format
+    GraphDisplayDOT(dig03);
+    printf("\n");
+
+    GraphCheckInvariants(dig03);
+
+    // Bellman-Ford Algorithm
     InstrReset();
-    GraphBellmanFordAlgDestroy(&BF_result);
-  }
+    InitializeInstrumentation();
 
-  // What kind of graph is g01?
-  Graph* g01 = GraphCreate(6, 0, 0);
-  GraphAddEdge(g01, 0, 5);
-  GraphAddEdge(g01, 2, 4);
-  GraphAddEdge(g01, 2, 3);
-  GraphAddEdge(g01, 1, 2);
-  GraphAddEdge(g01, 0, 1);
-  GraphAddEdge(g01, 3, 4);
-  GraphAddEdge(g01, 3, 5);
-  GraphAddEdge(g01, 0, 2);
-  printf("The graph:\n");
-  // Displaying in DOT format
-  GraphDisplayDOT(g01);
-  printf("\n");
+    // Consider each vertex as a start vertex
+    for (unsigned int i = 0; i < GraphGetNumVertices(dig03); i++) {
+        double startTime = cpu_time();
+        GraphBellmanFordAlg* BF_result = GraphBellmanFordAlgExecute(dig03, i);
+        double endTime = cpu_time();
 
-  GraphCheckInvariants(g01);
+        printf("The shortest path tree rooted at %u\n", i);
+        GraphBellmanFordAlgDisplayDOT(BF_result);
+        printf("\n");
 
-  // Bellman-Ford Algorithm
+        printf("Execution Time: %.6f seconds\n", endTime - startTime);
+        GraphBellmanFordAlgDestroy(&BF_result);
+    }
 
-  InitializeInstrumentation();
+    InstrPrint();
 
-  // Consider each vertex as a start vertex
-  for (unsigned int i = 0; i < 6; i++) {
-    GraphBellmanFordAlg* BF_result = GraphBellmanFordAlgExecute(g01, i);
+    GraphDestroy(&g01);
+    GraphDestroy(&dig01);
+    GraphDestroy(&dig03);
 
-    printf("The shortest path tree rooted at %u\n", i);
-    GraphBellmanFordAlgDisplayDOT(BF_result);
-    printf("\n");
-
-    GraphBellmanFordAlgDestroy(&BF_result);
-  }
-
-  InstrPrint();
-
-  // Reading a directed graph from file
-  FILE* file = fopen("DG_2.txt", "r");
-  Graph* dig03 = GraphFromFile(file);
-  fclose(file);
-  // Displaying in DOT format
-  GraphDisplayDOT(dig03);
-  printf("\n");
-
-  GraphCheckInvariants(dig03);
-
-  // Bellman-Ford Algorithm
-
-  InstrReset();
-  InitializeInstrumentation();
-
-  // Consider each vertex as a start vertex
-  for (unsigned int i = 0; i < GraphGetNumVertices(dig03); i++) {
-    GraphBellmanFordAlg* BF_result = GraphBellmanFordAlgExecute(dig03, i);
-
-    printf("The shortest path tree rooted at %u\n", i);
-    GraphBellmanFordAlgDisplayDOT(BF_result);
-    printf("\n");
-
-    GraphBellmanFordAlgDestroy(&BF_result);
-  }
-
-  InstrPrint();
-
-  GraphDestroy(&g01);
-  GraphDestroy(&dig01);
-  GraphDestroy(&dig03);
-
-  return 0;
+    return 0;
 }
